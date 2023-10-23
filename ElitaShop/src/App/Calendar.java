4b06652567c4327904721d01e4f767a92cb59f73
Furmi37 <147946906+Furmi37@users.noreply.github.com>
2023-10-23 12:01:01 +0200

    update

--- ElitaShop/src/App/Calendar.java
+++ ElitaShop/src/App/Calendar.java
@@ -1,21 +1,20 @@
-package App;
-
-import Data.Event;
-import Data.Job;
-import Data.Running;
+package Data;
 
 public class Calendar {
     public static void main(String[] args) {
         Event event = new Event();
-        event.addEvent("Parapetówka","21.10.23", "19:00 ","ul. Orlińskiego");
-        event.printInfo();
-        event.addReminder();
+        File file = new File();
+////        event.addEvent("Parapetówka","21.10.23", "19:00 ","ul. Orlińskiego");
+//        event.printInfo();
+//        event.addReminder();
+//
+//        Running run= new Running("Running","21.10.23","16:00","Park Lotników", 3,20);
+//        run.printInfo();
+//
+//        Event job = new Job("Work", "24.10.23", "8:00","Pcim","Day",20);
+//        job.printInfo();
+        file.info();
 
-        Running run= new Running("Running","21.10.23","16:00","Park Lotników", 3,20);
-        run.printInfo();
-
-        Event job = new Job("Work", "24.10.23", "8:00","Pcim","Day",20);
-        job.printInfo();
 
 
 
--- ElitaShop/src/App/File.java
+++ ElitaShop/src/App/File.java
@@ -1,2 +1,40 @@
-package App;public class File {
+package Data;
+
+import Data.Event;
+
+import java.util.Scanner;
+
+public class File {
+    public void info (){
+        int option = 1;
+        do {
+            System.out.println("Calendar");
+            System.out.println("Pick a option: ");
+            System.out.println("0 - exit the app");
+            System.out.println("1 - add event");
+            System.out.println("2 - add reminder to event");
+            System.out.println("3 - delete event");
+            System.out.println("4 - show all saved events");
+
+            Event event = new Event();
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Choose an option: ");
+            option = sc.nextInt();
+            System.out.println();
+
+            if (option == 0) {
+                System.out.println("Closing Calendar");
+            } else if (option == 1) {
+                event.addEvent();
+            } else if (option == 2) {
+                event.addReminder();
+            }else if (option == 3){
+                event.deleteEvent();
+            }else if ( option == 4 ){
+                event.eventList(event.getEvents());
+            }
+        }while(option != 0);
+    }
+
 }
