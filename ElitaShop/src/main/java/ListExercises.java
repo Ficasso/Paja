7735e5a72ab346a87422e8b3376bd51f84783149
Ficasso <pawel.filiciak@gmail.com>
2023-10-26 10:57:20 +0200

    Zadanka Listy

---
+++ ElitaShop/src/main/java/ListExercises.java
@@ -0,0 +1,26 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExercises {
+
+    private ArrayList<Integer> list = (ArrayList<Integer>) List.of(1,23,5,7,2,4,7,8,3);
+
+
+    public int sumElements(){
+        return 0;
+    }
+
+    public void addEvenElementsOfList(){
+
+    }
+
+    public int[] getElementsLessThan(int x){
+        return null;
+    }
+
+    public List<Integer> deleteAndReturnElementsLessThan(int x){
+        return null;
+    }
+}
